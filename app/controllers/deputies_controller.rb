b1b4f65992e44e2548f33256b60655fbf18448e5
negres <nataliaknegres@gmail.com>
2022-08-08 20:59:46 -0300

    feat: create deputies controller

--- app/controllers/deputies_controller.rb
+++ app/controllers/deputies_controller.rb
@@ -1,2 +1,20 @@
 class DeputiesController < ApplicationController
+  respond_to :json
+
+  def index
+    @deputies = Deputy.all.order(:name)
+
+    render status: :ok
+  end
+
+  def show
+    @deputy = Deputy.find(params[:id])
+
+    render status: :ok
+  end
+
+  def expenses
+    @deputy = Deputy.find(params[:id])
+    @expenses = Expense.where(deputy: @deputy).order(net_value: :desc).includes([:provider])
+  end
 end
