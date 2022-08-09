0fabb5601abf9cfaed705f54d019d403a9f3540c
negres <nataliaknegres@gmail.com>
2022-08-08 21:17:00 -0300

    fix: csv reader

--- app/controllers/import_file_controller.rb
+++ app/controllers/import_file_controller.rb
@@ -2,7 +2,7 @@ class ImportFileController < ApplicationController
   def import_file
     file = params[:file].path.to_s
 
-    CsvWorkers::CreateDataWorker.perform_in(1.second, file)
+    CsvReaderWorker::ReaderFileWorker.perform_in(file)
     render status: :created, json: { messsage: 'arquivo criado com sucesso!' }
   end
 end
