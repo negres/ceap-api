a4273dd082a40f88cfc8c6ad8ee17f2962f5c2cc
negres <nataliaknegres@gmail.com>
2022-08-09 19:34:56 -0300

    fix: application

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,19 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails/all"
+require 'rails'
+# Pick the frameworks you want:
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+require 'active_storage/engine'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_mailbox/engine'
+require 'action_text/engine'
+require 'action_view/railtie'
+require 'action_cable/engine'
+# require "sprockets/railtie"
+require 'rails/test_unit/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -8,21 +21,35 @@ Bundler.require(*Rails.groups)
 
 module CeapApi
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 6.1
+    config.action_mailer.delivery_job = 'ActionMailer::MailDeliveryJob'
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.time_zone = 'Brasilia'
+
+    config.encoding = 'utf-8'
+
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    config.i18n.load_path += Dir[Rails.root.join('my/locales/*.{rb,yml}').to_s]
+    config.i18n.enforce_available_locales = false
+    config.i18n.available_locales = ['pt-BR']
+    config.i18n.default_locale = :'pt-BR'
+
+
+    # Add Gzip compression to response headers
+    config.middleware.use Rack::Deflater
+
     config.active_job.queue_adapter = :sidekiq
   end
 end
